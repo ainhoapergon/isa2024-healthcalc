592a3334e73a580ada132f97229abbaf9f26a017
ainhoaPer <ainhoapergon@uma.es>
2024-05-09 15:49:03 +0200

    Main para comprobar patrón Singleton

--- src/main/java/healthcalc/gui/Main.java
+++ src/main/java/healthcalc/gui/Main.java
@@ -9,14 +9,21 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HealthCalcImpl modelo = HealthCalcImpl.getInstancia();
-					CalcVista vista = new CalcVista();
-					vista.setVisible(true);
-					ResultVista resultVista = new ResultVista();
-					resultVista.setVisible(false);
-					Controlador controlador = new Controlador(modelo, vista, resultVista);
-					vista.registrarControlador(controlador);
-					resultVista.registrarControlador(controlador);
+					// Comprobamos si el Singleton está implementado correctamente.
+					HealthCalcImpl modelo1 = HealthCalcImpl.getInstancia();
+                    HealthCalcImpl modelo2 = HealthCalcImpl.getInstancia();
+
+                    System.out.println("Referencia de modelo1: " + System.identityHashCode(modelo1));
+                    System.out.println("Referencia de modelo2: " + System.identityHashCode(modelo2));
+                    System.out.println("¿Es la misma instancia? " + (modelo1 == modelo2));
+
+					// CalcVista vista = new CalcVista();
+					// vista.setVisible(true);
+					// ResultVista resultVista = new ResultVista();
+					// resultVista.setVisible(false);
+					// Controlador controlador = new Controlador(modelo, vista, resultVista);
+					// vista.registrarControlador(controlador);
+					// resultVista.registrarControlador(controlador);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
